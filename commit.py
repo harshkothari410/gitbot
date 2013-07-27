c95dd28e785389edfbe6f9dd78a95dd2a1479676
harshkothari410 <harshkothari410@gmail.com>
2013-07-28 03:30:57 +0530

    added json object of commit detail facility
    added json object of commit detail facility by extracting the data from
    commit msg

--- commit.py
+++ commit.py
@@ -9,18 +9,57 @@ def commit(path, directory):
 	finalpath = path + '/' + directory
 	print file_list[:len(file_list) - 1]
 	commit_dict = {}
+	keys = ['commit','author','data','message','long_message']
 	if option == '1':
 		for f in file_list[:len(file_list) - 1]:
 			command = 'git log ' + f
 			data = gitdata(finalpath,command)
-			commit_dict[f] = data
+			#datalist = data.count('Author:')
+			if data.count('Author:') > 1:
+				data = data.split('commit ')
+				data.remove('')
+				sub_dict = []
+				for s in data:
+					values = dataExtract(s)
+					temp = createDict(keys, values)
+					sub_dict.append(temp)
+			else:
+				values = dataExtract(data)
+				sub_dict = createDict(keys, values)
+			#print data
+			commit_dict[f] = sub_dict
 	if option == '2':
 		for f in file_list[:len(file_list) - 1]:
 			command = 'git log --diff-filter=A -- ' + f
-			#print command
 			data = gitdata(finalpath,command)
-			commit_dict[f] = data
+			values = dataExtract(data)
+			sub_dict = createDict(keys, values)
+			commit_dict[f] = sub_dict
+	#print commit_dict
 	return commit_dict
+
+def dataExtract(data):
+	datalist = data.split('\n')
+	value = []
+	count = 0
+	for d in datalist:
+		d = d.replace('    ','')
+		if d == '':
+			continue
+		if count > 4:
+			temp = d
+			value[4] = value[4] + ' ' + temp
+		else:
+			value.append(d)
+		count = count + 1
+	if value[0].find('commit ') == 0: 
+		value[0] = value[0][len('commit '):]
+	value[1] = value[1][len('Author: '):]
+	value[2] = value[2][len('Date:   '):]
+	return value	
+
+def createDict(keys, values):
+	return dict(zip(keys, values))
 
 if __name__ == '__main__':
 	data = commit(sys.argv[1],sys.argv[2])
